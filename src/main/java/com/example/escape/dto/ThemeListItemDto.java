343c2c6b47da1eb797aafb802b938da64a497283
Minsung Oh <qww1552@naver.com>
2023-09-24 19:20:11 +0900

    feat: 전체 조회 시 theme dto

---
+++ src/main/java/com/example/escape/dto/ThemeListItemDto.java
@@ -0,0 +1,15 @@
+package com.example.escape.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ThemeListItemDto {
+
+    private String thumbnail;
+    private String title;
+    private String location;
+
+}
