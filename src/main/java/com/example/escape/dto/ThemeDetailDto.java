16e61e49fd65cac5f818ac58cb2f423ae48b283e
Minsung Oh <qww1552@naver.com>
2023-10-18 22:21:50 +0900

    feat: theme detail dto

--- src/main/java/com/example/escape/dto/ThemeDetailDto.java
+++ src/main/java/com/example/escape/dto/ThemeDetailDto.java
@@ -1,7 +1,20 @@
 package com.example.escape.dto;
 
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
 public class ThemeDetailDto {
 
-    // TODO: 2023-09-30  필드 정의
+    private Long id;
+    private String title;
+    private String thumbnail;
+    private String genre;
+    private String store;
+    private String address;
+    private Integer playTime;
+    private String recommendedPeople;
+    private Integer maximumPeople;
 
 }
