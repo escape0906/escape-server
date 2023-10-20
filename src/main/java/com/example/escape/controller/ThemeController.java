8b0e501c2313e071e09ba7946d47bb922e5b9827
Minsung Oh <qww1552@naver.com>
2023-10-21 00:12:48 +0900

    fix: 오타 수정

--- src/main/java/com/example/escape/controller/ThemeController.java
+++ src/main/java/com/example/escape/controller/ThemeController.java
@@ -9,10 +9,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/api/themes")
+@RestController
+@RequestMapping("/api/themes")
 public class ThemeController {
 
     private final ThemeService themeService;
